c867fdaae04b9c67103ca9aef0d92cb8590d58fb
SKG360 <samuel.giles@colorado.edu>
2018-07-02 22:50:39 -0600

    Guess Class update

---
+++ test/guess_test.rb
@@ -0,0 +1,17 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/guess.rb'
+require './lib/card.rb'
+
+
+
+class GuessTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("10 of Hearts", card)
+    guess = Guess.new("10 of Hearts", card)
+    assert_instance_of Guess, guess
+  end
+
+end
