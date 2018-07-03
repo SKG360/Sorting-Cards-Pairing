92491ebcd98e1092d447ff95538d7e11b59f9607
SKG360 <samuel.giles@colorado.edu>
2018-07-02 22:02:35 -0600

    Initial Guess Class

---
+++ lib/guess.rb
@@ -0,0 +1,24 @@
+# require './lib/card.rb'
+
+class Guess
+
+
+  def initialize(response, card)
+
+    @response = response
+    @card = card
+
+  end
+
+  def response
+    @response
+  end
+
+  def card
+    @card
+  end
+
+  guess = Guess.new("10 of Hearts", @card)
+  p guess.response
+  p guess.card
+end
