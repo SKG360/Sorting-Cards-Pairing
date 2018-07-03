8b4a27fd7ab9c3152600448d8595ee37f347b3a0
SKG360 <samuel.giles@colorado.edu>
2018-07-03 17:10:33 -0600

    Update guess test

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -5,16 +5,24 @@ require './lib/guess.rb'
 require './lib/card.rb'
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b5d539ee546746ec3bf5b0671c157feef725b43e
 class GuessTest < Minitest::Test
 
   def test_it_exists
-    card = Card.new("10 of Hearts", card)
+    card = Card.new("10", "Hearts")
     guess = Guess.new("10 of Hearts", card)
     assert_instance_of Guess, guess
+  end
+
+  def test_response
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal "10 of Hearts", guess.response
+  end
+
+  def test_card
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal card, guess.card
   end
 
 end
