e4747c66df621c86cfccc6316e54a1f5e7921200
SKG360 <samuel.giles@colorado.edu>
2018-07-04 19:59:59 -0600

    Updated Round and Guess files

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,12 +1,29 @@
-# require './lib/card.rb'
+require './lib/card.rb'
 
 class Guess
 
-attr_reader :card, :response 
+attr_reader :card, :response
 
   def initialize(response, card)
     @response = response
     @card = card
   end
+
+  def response_two_diamonds
+
+  end
+
+  def correct?
+    @response == "#{@card.value} of #{@card.suit}"
+  end
+
+  def feedback
+    if correct? == false
+      "Incorrect"
+    else
+      "Correct!"
+    end
+  end
+
 
 end
